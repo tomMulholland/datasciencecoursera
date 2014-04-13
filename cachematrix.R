c622b5abfa5f43fb5750a36594accd41e740acc2
Tom Mulholland <tom.c.mulholland@gmail.com>
2014-04-13 15:42:20 -0500

    solved homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions were created as part of the Coursera course R Programming
+## Their goal is to reduce computation time for matrix inversions
 
-## Write a short comment describing this function
+## makeCacheMatrix makes a special matrix object which holds inverted matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve tests if an inverted matrix solution is already stored
+## if not, it calculates and stores the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
